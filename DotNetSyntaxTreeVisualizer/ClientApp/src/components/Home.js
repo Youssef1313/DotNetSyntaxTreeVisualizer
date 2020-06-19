15866ee64be555721d5f3476b1ee547e40c5d8fa
Youssef Victor <youssefvictor00@gmail.com>
2020-06-19 20:36:14 +0200

    Make path straight

--- DotNetSyntaxTreeVisualizer/ClientApp/src/components/Home.js
+++ DotNetSyntaxTreeVisualizer/ClientApp/src/components/Home.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
 import Tree from 'react-d3-tree';
 
-const svgSquare = {
-    shape: 'rect',
-    shapeProps: {
-        width: 20,
-        height: 20,
-        x: -10,
-        y: -10,
-    }
-}
-
 const containerStyles = {
     width: '100%',
     height: '100vh',
@@ -57,7 +47,7 @@ export class Home extends Component {
             <React.Fragment>
                 <textarea defaultValue={helloWorldCode} onChange={this.handleChanged} style={{ height: '230px', width: '100%' }} />
                 <div id="treeWrapper" style={containerStyles} ref={tc => (this.treeContainer = tc)}>
-                    <Tree data={this.state.treeJson} nodeSvgShape={svgSquare} orientation="vertical" translate={this.state.translate} />
+                    <Tree data={this.state.treeJson} orientation="vertical" translate={this.state.translate} pathFunc="straight" />
                 </div>
             </React.Fragment>
         );
